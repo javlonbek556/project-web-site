f7df28fd4f78d68c847210c626cc5581e58dc7f3
javlonbek556 <javlonzdess@gmail.com>
2025-03-07 17:43:44 +0500

    main controllers in this file

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    public function jobDetails()
+    {
+        return view('work');
+    }
+
+    public function jobListings()
+    {
+        return view('jobs');
+    }
+}
